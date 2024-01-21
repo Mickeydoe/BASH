3cabaf25d58cf8332bf4304132b2d40bec9e461e
Mickeydoe <michaeldanquah615@gmail.com>
2024-01-21 21:38:10 +0000

    loops

---
+++ loops.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for names in Michael Kofi Reagan Albert Stephan Kingsley
+do
+	echo $names
+done
+
+
+for i in {0..100}
+do
+	echo $i
+done
+
+for ((j=0; j<=10; j++))
+do
+	echo $j
+done
