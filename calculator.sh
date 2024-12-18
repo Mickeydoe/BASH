1f7e0303c0c5965e32c59be9b25cbe71a49b8766
Mickeydoe <michaeldanquah615@gmail.com>
2024-12-18 23:07:43 +0000

    New calculator commits

--- calculator.sh
+++ calculator.sh
@@ -8,3 +8,28 @@ read num1
 read num2
 
 
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Division"
+echo "4. Multiplication"
+
+echo "Which operation would you like to perform on the two numbers"
+read choice
+
+if [ $choice = 1 ]; then
+    result=$((num1 + num2))
+    echo "The result of addition is: $result"
+
+elif [ $choice = 2 ]; then       
+    result=$((num2 - num1))
+    echo "The result of ubtraction is: $result"
+
+elif [ $choice = 3 ]; then 
+    result=$((num2/num1))
+    echo "The result of division is: $result"
+
+elif [ choice eq 4]; then
+    result=$((num1 * num2))
+    echo "The result of multiplication is $result"
+
+fi
